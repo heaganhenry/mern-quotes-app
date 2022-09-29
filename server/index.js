0adcb25b23179b61d67b8b445308e5343be7ddc7
Heagan Henry <heaganhenry@gmail.com>
2022-09-29 11:13:50 -0400

    Setup api/register handler

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/hello', (req, res) => {
-    res.send('hello world')
+app.post('/api/register', (req, res) => {
+    console.log(req.body);
+    res.json({ status: 'ok'})
 })
 
 app.listen(1337, () => {
