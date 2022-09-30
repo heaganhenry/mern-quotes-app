c95b0bef11e5c2709e084ddbb1aebf7c2b5bec16
Heagan Henry <heaganhenry@gmail.com>
2022-09-30 09:06:14 -0400

    Complete api/register handler

--- server/index.js
+++ server/index.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/mern-web-app')
 
-app.post('/api/register', (req, res) => {
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    res.json({ status: 'ok'})
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
 })
 
 app.listen(1337, () => {
